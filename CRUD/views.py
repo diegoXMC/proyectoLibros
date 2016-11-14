aa569371acf623ae1db472dce635a19251c3c7f2
diegoXMC <diegopalomilla@yahoo.com.ar>
2016-11-14 15:58:14 -0600

    primer vista y plantilla html

--- CRUD/views.py
+++ CRUD/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Libros
 
-# Create your views here.
+def show_libros(request):
+    libro = Libros.objects.all()
+    return render(request, 'libros_list.html', {'libro': libro})
