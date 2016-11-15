6781667ffa6a119a651cc630361977c0a7ead3d2
diegoXMC <diegopalomilla@yahoo.com.ar>
2016-11-15 17:11:13 -0600

    creacion de la extension de la plantilla para usar base y del detalle del libro

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.show_libros),
     url(r'^inicio/$', views.show_libros),
+    url(r'^detalle/(?P<pk>[0-9]+)/$', views.detale_libro),
 ]
--- CRUD/views.py
+++ CRUD/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Libros
 
 def show_libros(request):
     libro = Libros.objects.all()
     return render(request, 'libros_list.html', {'libro': libro})
+
+def detalle_libro(request, pk):
+    libro = get_object_or_404(Libros, pk=pk)
+    return render(request, 'post_detail.html', {'libro': libro})
