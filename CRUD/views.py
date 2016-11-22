b0d8c1657c0278cec2047d41bd09af63e3c52dba
diegoXMC <diegopalomilla@yahoo.com.ar>
2016-11-22 17:36:11 -0600

    ya full, solo unas pequeñas cosas de diseño

--- CRUD/views.py
+++ CRUD/views.py
@@ -11,6 +11,7 @@ from django.template.context import RequestContext
 from django.http.response import HttpResponseRedirect
 
 def nuevo_usuario(request):
+    usuario = request.user
     if not request.user.is_anonymous():
         return HttpResponseRedirect('/libros/inicio')
     if request.method=='POST':
@@ -20,9 +21,10 @@ def nuevo_usuario(request):
             return HttpResponseRedirect('/libros/ingresar')
     else:
         formulario = UserCreationForm()
-    return render_to_response('nuevo_usuario.html', {'formulario':formulario}, context_instance=RequestContext(request))
+    return render_to_response('nuevo_usuario.html', {'formulario':formulario, 'usuario':usuario}, context_instance=RequestContext(request))
 
 def ingresar(request):
+    usuario = request.user
     if not request.user.is_anonymous():
         return HttpResponseRedirect('/libros/inicio')
     if request.method=='POST':
@@ -42,7 +44,7 @@ def ingresar(request):
                 return HttpResponseRedirect('/libros/inicio')
     else:
         formulario = AuthenticationForm()
-    return render_to_response('ingresar.html', {'formulario':formulario}, context_instance=RequestContext(request))
+    return render_to_response('ingresar.html', {'formulario':formulario, 'usuario':usuario}, context_instance=RequestContext(request))
 
 @login_required(login_url='/libros/ingresar')
 def cerrar(request):
@@ -52,17 +54,18 @@ def cerrar(request):
 #todo lo referente a libros
 def show_libros(request):
     libro = Libros.objects.all()
-    usuario = request.user.is_anonymous()
-    return render(request, 'libros_list.html', {'libro': libro, 'ususario':usuario})
+    usuario = request.user
+    return render(request, 'libros_list.html', {'libro': libro, 'usuario':usuario})
 
 def libro_detalle(request, pk):
     libro = get_object_or_404(Libros, pk=pk)
     autor = Asignacion2.objects.filter(Libro = pk)
-    usuario = request.user.is_anonymous()
+    usuario = request.user
     return render(request, 'libros_detalle.html', {'libro': libro, 'autor':autor, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def libro_editar(request, pk):
+    usuario = request.user
     libro = get_object_or_404(Libros, pk=pk)
     if request.method == "POST":
         formulario = LibroForm(request.POST, instance=libro)
@@ -73,17 +76,17 @@ def libro_editar(request, pk):
             return redirect('CRUD.views.libro_detalle', pk=libro.pk)
     else:
         formulario = LibroForm(instance=libro)
-    return render(request, 'nuevo_libro.html', {'formulario': formulario})
+    return render(request, 'nuevo_libro.html', {'formulario': formulario, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def libro_borrar(request, pk):
     libro = get_object_or_404(Libros, pk=pk)
     libro.delete()
-    return HttpResponseRedirect('/libros/inicio')
+    return render(request, 'libros_list.html', {'libro': libro, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def libro_nuevo(request):
-    usuario = request.user.is_anonymous()
+    usuario = request.user
     if request.method == "POST":
         formulario = LibroForm(request.POST)
         if formulario.is_valid():
@@ -103,38 +106,40 @@ def libro_nuevo(request):
 #todo lo referente a autores
 def show_autores(request):
     autor = Autores.objects.all()
-    usuario = request.user.is_anonymous()
-    return render(request, 'autores_list.html', {'autor': autor, 'ususario':usuario})
+    usuario = request.user
+    return render(request, 'autores_list.html', {'autor': autor, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def autores_editar(request, pk):
+    usuario = request.user
     autor = get_object_or_404(Autores, pk=pk)
     if request.method == "POST":
-        formulario = AutorForm(request.POST, instance=libro)
+        formulario = AutorForm(request.POST, instance=autor)
         if formulario.is_valid():
             autor = formulario.save(commit=False)
             autor.save()
             return redirect('CRUD.views.show_libros')
     else:
         formulario = AutorForm(instance=autor)
-    return render(request, 'nuevo_autor.html', {'formulario': formulario})
+    return render(request, 'nuevo_autor.html', {'formulario': formulario, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def autores_borrar(request, pk):
+    usuario = request.user
     autor = get_object_or_404(Autores, pk=pk)
     autor.delete()
-    return HttpResponseRedirect('/libros/inicio')
+    return render(request, 'autores_list.html', {'autor': autor, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def autores_nuevo(request):
-    usuario = request.user.is_anonymous()
+    usuario = request.user
     if request.method == "POST":
         formulario = AutorForm(request.POST)
         if formulario.is_valid():
             cre = request.user.id
             autor = formulario.save(commit=False)
             autor.save()
-            return redirect('CRUD.views.show_libros')
+            return redirect('CRUD.views.show_autores')
     else:
         formulario = AutorForm()
     return render(request, 'nuevo_autor.html', {'formulario': formulario, 'usuario':usuario})
@@ -142,11 +147,12 @@ def autores_nuevo(request):
 #todo lo referente con los generos
 def show_generos(request):
     genero = Generos.objects.all()
-    usuario = request.user.is_anonymous()
+    usuario = request.user
     return render(request, 'generos_list.html', {'genero': genero, 'ususario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def generos_editar(request, pk):
+    usuario = request.user
     genero = get_object_or_404(Generos, pk=pk)
     if request.method == "POST":
         formulario = GeneroForm(request.POST, instance=genero)
@@ -156,19 +162,19 @@ def generos_editar(request, pk):
             return redirect('CRUD.views.show_generos')
     else:
         formulario = GeneroForm(instance=genero)
-    return render(request, 'nuevo_genero.html', {'formulario': formulario})
+    return render(request, 'nuevo_genero.html', {'formulario': formulario, 'usuario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def generos_borrar(request, pk):
     genero = get_object_or_404(Generos, pk=pk)
     genero.delete()
-    return HttpResponseRedirect('/libros/inicio')
+    return render(request, 'generos_list.html', {'genero': genero, 'ususario':usuario})
 
 @login_required(login_url='/libros/ingresar')
 def generos_nuevo(request):
-    usuario = request.user.is_anonymous()
+    usuario = request.user
     if request.method == "POST":
-        formulario = AutorForm(request.POST)
+        formulario = GeneroForm(request.POST)
         if formulario.is_valid():
             genero = formulario.save(commit=False)
             genero.save()
